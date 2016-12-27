164912aa649d681871f304cf45898ed5e0878f7c
komal dubey <kkdubey1989@gmail.ccom>
2016-12-27 22:36:35 +0530

    app-bar added

---
+++ src/app/ui/AppBar/app-bar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar',
+  styleUrls: [`app/ui/AppBar/app-bar.style.css`],
+  templateUrl: `app/ui/AppBar/app-bar.tpl.html`
+})
+export class AppBar {
+}
